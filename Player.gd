fe376b6c7c3b5c13745133c76a82b8f3d2c1f6b0
alex <rwxr@foxmail.com>
2022-06-23 12:58:57 +0800

    mob and timers

--- Player.gd
+++ Player.gd
@@ -1,9 +1,18 @@
 extends Area2D
 
+# 定义碰撞信号
+signal hit
+
 export var speed = 100
 var screen_size
 
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
+
 func _ready():
+	# 初始化时，隐藏玩家
 	hide()
 	screen_size = get_viewport_rect().size
 	
@@ -41,3 +50,12 @@ func _process(delta):
 	elif velocity.y != 0:
 		$AnimatedSprite.animation = "up"
 		$AnimatedSprite.flip_v = velocity.y > 0
+
+
+func _on_Player_body_entered(body):
+	# 被击中后消失
+	hide()
+	# 触发碰撞信号
+	emit_signal("hit")
+	# 如果在引擎的碰撞处理过程中禁用区域的碰撞形状可能会导致错误。使用 set_deferred() 告诉 Godot 等待可以安全地禁用形状时再这样做。
+	$CollisionShape2D.set_deferred("disabled", true)
